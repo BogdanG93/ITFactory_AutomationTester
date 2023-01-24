f59ecc10c95165baededfad2aa060f43d1fddb8a
BogdanG93 <bogdan_geagu93@yahoo.com>
2023-01-24 12:04:27 +0200

    update tema01

--- Tema01 optional/main.py
+++ Tema01 optional/main.py
@@ -1,51 +1,31 @@
 # 1. Exercițiu:
 # - citește de la tastatură un string de dimensiune impară;
 # - afișează caracterul din mijloc.
-print('Ex.1 optional:')
-palindrom = 'aerisirea'  # we cheat because our_variable > input_from_keyboard
-print(f'Cuvantul "{palindrom}" este cel mai lung panlindrom din limba romana si are {len(palindrom)} caractere.')
-char_mijloc_palindrom = round((len(palindrom))/2)
-print('Singurul caracter care nu se afla in cuvant si se afla in mijlocul acestuia este: ', palindrom[char_mijloc_palindrom])
-# print(palindrom[(round((len(palindrom))/2))])
-print()
+propoz = input("Scrie un string de dimensiune impara.\n")
+print("Caracterul din mijlocul string-ului este:", propoz[(len(propoz)//2)])
+
 # 2. Folosind assert, verifică dacă un string este palindrom.
-print('Ex.2 optional:')
-print('Pentru a verifica daca un cuvant este palindrom, trebuie sa-l comparam cu reversul acestuia.')
-assert palindrom == palindrom[::-1]
-print('Daca vezi acest mesaj, inseamna ca', palindrom, 'este palindrom.')
-print()
+palindrom = "aerisirea"
+assert palindrom == palindrom[::-1], "Cuvantul nu este palingdrom."
+
 # 3. Folosind o singură linie de cod :
 # - citește un string de la tastatură (ex: 'alabala portocala');
 # - salvează fiecare cuvânt într-o variabilă;
 # - printează ambele variabile pentru verificare.
-print('Ex.3 optional:')
-# Prima incercare de aproape o singura linie de cod
-#a,b = tuple((input("Scrie o propozitie care iti trece prin minte."), input("Scrie o propozitie care iti trece prin minte.").split()))
-#print(a, b)
-def o_singura_linie_de_cod():
-    l = input("Scrie o propozitie care iti trece prin minte.")
-    m = l.split()
-    # a, b, c = m
-    print(f'Ati introdus propozitia "{l}", iar cu ajutorul acestei "singure linii de cod", am transformat-o in lista {m}')
-    # print(f'si variabilele {a}, {b}, {c}.')
-
-o_singura_linie_de_cod()
+string = input("Scrie o propozitie care iti trece prin minte.\n")
+x, y = string.split(" ")
+print("Primul cuvant este:", x)
+print("Al doilea cuvant este:", y)
 
 #4. Exercițiu:
 # - citește un string de la tastatură (ex: alabala portocala);
 # - salvează primul caracter într-o variabilă - indiferent care este el, încearcă cu 2 stringuri diferite;
 # - capitalizează acest caracter peste tot, mai puțin pentru primul și ultimul caracter => alAbAlA portocAla.
-print('Ex.4 optional:')
-string_original = input("Scrie o propozitie random.")
-char0 = string_original[0].lower()
-char_minus_1 = string_original[-1].lower()
-string_modificat = string_original.replace(char0, char0.upper())
-lista_string_modificat = list(string_modificat)
-lista_string_modificat[0] = char0
-lista_string_modificat[-1] = char_minus_1
-string_modificat = ''.join(lista_string_modificat)
-print('Sirul introdus este', {string_original}, ', acesta transformandu-se in', {string_modificat}, '.')
-print()
+string = input("Scrie o propozitie random.\n")
+prima_litera = string[0]
+string_modificat = prima_litera + string[1:(len(string)-1)].replace(prima_litera, prima_litera.upper()) \
+                   + string[len(string)-1]
+print(string_modificat)
 
 # 5.Exercițiu:
 # - citește un user de la tastatură;
@@ -55,11 +35,7 @@ print()
 # afișeze corect.
 # eg: parola abc => ***
 # parola abcd => ****
-print('Ex.5 optional:')
 username = input('Creati un user.')
 parola = input('Setati parola.')
 parola_ascunsa = len(parola) * '*'
 print('Ati creat user-ul', username, 'cu parola', parola_ascunsa, '.')
-
-
-
--- Tema01/main.py
+++ Tema01/main.py
@@ -3,22 +3,20 @@
 
 Variabila este informatia pe care o stocam intr-un spatiu alocat de memorie, in scopul manipularii acesteia.
 
-Practic ne putem gandi la spatiu ca fiind un dulap, spatiul alocat variabilei ca fiind un obiect care sa contina
-informatia, de genul o cutie, numele variabilei ar fi labelul de pe cutie, iar in interiorul cutiei s-ar afla informatia.
+Practic ne putem gandi la spatiu ca fiind un dulap, spatiul alocat variabilei ca fiind un obiect care sa
+contina informatia, de genul o cutie, numele variabilei ar fi labelul de pe cutie, iar in interiorul cutiei
+s-ar afla informatia.
 """
 
 # 2. Declară și initializează câte o variabilă din fiecare din următoarele tipuri de variabilă :
-
 tema = 'Azi ne facem tema.'   # str
-exercitii = 10 # int
-ora = 18.23 # float
-facem_toata_tema = False # bool
+exercitii = 10                # int
+ora = 18.23                   # float
+facem_toata_tema = False      # bool
 
 # 3. Utilizează funcția type pentru a verifica dacă au tipul de date așteptat.
 #                               +
 # 5. Folosește print() și printează în consola 4 propoziții folosind cele 4 variabile.
-print('Ex. 3 + 5:')
-print()
 print('Variabilele declarate anterior sunt de tipul: ')
 print(f'tema: {tema}--> ', type(tema))
 print(f'exercitii: {exercitii}-->', type(exercitii))
@@ -26,46 +24,29 @@ print(f'ora: {ora}-->', type(ora))
 print(f'facem_toata_tema: {facem_toata_tema}-->', type(facem_toata_tema))
 print(tema + ' Avem ' + str(exercitii) + ' exercitii de rezolvat. Avand in vedere faptul ca este ora ' + str(ora) +
       ', credeti ca intr-o ora le putem rezolva pe toate? R: ' + str(facem_toata_tema))
-print()
-print()
 
 # 4. Rotunjește ‘float’-ul folosind funcția round() și salvează această modificare în aceeași variabilă (suprascriere):
 # - Verifică tipul acesteia.
-print('Ex. 4:')
-print()
 print(f'Variabila "ora" are valoarea de: {ora}')
 ora = round(ora)
 print(f'Dupa rotunjire, ea are valoarea: {ora} si este de tipul-->', type(ora))
-print()
-print()
 
 # 6. Citește de la tastatură:
 # - numele;
 # - prenumele.
 # Afișează: 'Numele complet are x caractere'.
-print('Ex. 6:')
-print()
-numele = input(print('Scrie-ti numele: '))
-prenumele = input(print('Scrie-ti prenumele: '))
-nume_complet = numele + ' ' + prenumele
-print('Nume complet: ', nume_complet)
-length_nume_complet = len(nume_complet)
-print(f"Numele complet are {length_nume_complet} caractere")
-print()
-print()
+numele = input('Scrie-ti numele: ')
+prenumele = input('Scrie-ti prenumele: ')
+print(f"Numele complet are {len(numele) + len(prenumele)} caractere")
 
 # 7. Citește de la tastatură:
 # - lungimea;
 # - lățimea.
 # Afișează: 'Aria dreptunghiului este x'.
-print('Ex. 7:')
-print()
 print('Calculam aria dreptunghiului.')
-L = input('Alege lungimea dreptunghiului.')
-l = input('Alege latimea dreptunghiului.')
-print ('Aria dreptunghiului este: ', int(L) * int(l))
-print()
-print()
+lungimea = input('Alege lungimea dreptunghiului: ')
+latimea = input('Alege latimea dreptunghiului: ')
+print('Aria dreptunghiului este: ', int(lungimea) * int(latimea))
 
 # 8. Având stringul: 'Coral is either the stupidest animal or the smartest rock':
 # - afișează de câte ori apare cuvântul 'the';
@@ -73,26 +54,10 @@ print()
 # 9. Același string.
 # ● Afișează de câte ori apare cuvântul 'the';
 # ● Printează rezultatul.
-print('Ex. 8 + 9:')
-print()
-random_philosophy = 'Coral is either the stupidest animal or the smartest rock'
-print('Avem string-ul: ', random_philosophy,'.')
-print('Acesta are ', len(random_philosophy), ' de caractere.')
-print('Avand aceasta informatie, putem folosi functia .count pentru a afisa de cate ori se repeta cuvantul "the".')
-string_counted = random_philosophy.count('the', 0, len(random_philosophy))
-print('String-ul contine cuvantul "the" de ', string_counted, ' ori.')
-print()
-print()
+propoztie = 'Coral is either the stupidest animal or the smartest rock'
+print('String-ul contine cuvantul "the" de ', propoztie.count(' the '), ' ori.')
 
 # 10. Același string.
 # ● Folosiți un assert ca să verificați dacă acest string conține doar numere.
-print('Ex. 10:')
-print()
-string_has_numbers = random_philosophy.isnumeric()
-print(string_has_numbers)
-assert random_philosophy.isnumeric() == False
-print('Acest string nu conține doar numere.')
-assert random_philosophy.isnumeric() == True
-print('Acest string conține doar numere.')
-
-
+propoztie = 'Coral is either the stupidest animal or the smartest rock'
+assert propoztie.isnumeric() is True, "Propozitia nu contine doar cifre"
